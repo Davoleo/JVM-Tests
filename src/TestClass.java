bac56327d5f2d8a8b152dfef870b411a6b4e854c
Davoleo <davoleo2001@gmail.com>
2018-04-24 23:31:16 +0200

    First Commit

---
+++ src/TestClass.java
@@ -0,0 +1,13 @@
+
+//Packages per importare comandi
+//import java.lang.*;
+
+public class TestClass {
+
+    public static void main(String[] args){
+
+        //classe: System, al cui interno c'è l'esemplare out il quale ha print al suo interno
+        System.out.print(5 + 3);
+
+    }
+}
