e8fb5642609fe2cfa61278b9b81f9dd9e117697c
Davoleo <davoleo2001@gmail.com>
2018-04-25 00:23:45 +0200

    Variabili double, operatori aritmetici, Struttura workspace

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,7 +7,22 @@ public class TestClass {
     public static void main(String[] args){
 
         //classe: System, al cui interno c'è l'esemplare out il quale ha print al suo interno
-        System.out.print(5 + 3);
+        System.out.println(5 + 3);
+        System.out.println("ciao");
+        System.out.println(3 + 5 + "bella");
+        System.out.println(3.14);
+        System.out.println(3*5);
+        System.out.println(3.0/5); //<-- CAST   forzatura di tipo
+
+        //Double: 8 bytes, (64 bits)
+        double percentualeInteresse = 2.5, SommaDepositata = 1000;
+
+        //dopo un anno
+        SommaDepositata= sommaDepositata + (sommanDepositata/100) * PercentualeInteresse;
+        System.out.println("Dopo un anno avrai " + sommaDepositata + " €")
+
+
+
 
     }
 }
