213f3a40c53c21efb398341896fc10ef9775d31e
Davoleo <davoleo2001@gmail.com>
2018-05-10 15:54:38 +0200

    Costrutto while parte 2

--- src/costrutti/While.java
+++ src/costrutti/While.java
@@ -6,6 +6,7 @@
  **************************************************/
 
 package costrutti;
+import java.time.Clock;
 import java.util.Scanner;
 
 public class While {
@@ -35,27 +36,42 @@ public class While {
 
         //Somma dei numeri compresi tra A e B letti da tastiera
         Scanner tastiera = new Scanner(System.in);
-        int sommaA_B = 0;
-        int a = 0, b= 0;
+        int sommaA_B = 0;   //somma tra gli estremi
+        int a = 0, b= 0;    //estremi
 
         System.out.println("Parto da: ");
-        a= tastiera.nextInt();
+        a= tastiera.nextInt();      //richiesta del dato
 
         System.out.println("E arrivo fino a: ");
         b = tastiera.nextInt();
 
         if (b<a)
         {
+            System.out.println("va be scambierò io i due estremi brutto stronzo... " +
+                    "ma guarda te queste persone che inseriscono i dati al controrio");
             int temp = a;
             a=b;
             b=temp;
         }
         int cont2 = a;
-        while (cont2<=b);
-        {
+        while (cont2<=b)
+        /*{
             sommaA_B += cont2;
             cont2++;
+        }*/
+        //più ottimizzato
+        sommaA_B += cont2++;
+
+        System.out.println("la somma dei numeri tra " + a + " e " + b + " è: " + sommaA_B);
+
+        //far inserire dati da tastiera e sommarli fintanto che la somma è inferiore a 1000
+        int somma1000 = 0;
+        while(somma1000 < 1000)
+        {
+            System.out.println("Aggiungiamo un altro numero(" + somma1000 + "):");
+            somma1000 += tastiera.nextInt();
         }
+        System.out.println("Terminato con valore: " + somma1000);
 
     }
 }
