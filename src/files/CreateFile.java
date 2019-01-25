c23dcb1c0820605a7db1a850fbd763c77579f299
Davoleo <davoleo2001@gmail.com>
2019-01-25 18:21:38 +0100

    File Management Creating Reading Writing Infos

---
+++ src/files/CreateFile.java
@@ -0,0 +1,39 @@
+package files;
+
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 25/01/2019 / 17:57
+ * Class: CreateFile
+ * Project: JavaBase
+ * Copyright - © - Davoleo - 2018
+ **************************************************/
+
+class CreateFile {
+
+    private Formatter formatter;
+
+    void openFile()
+    {
+        try {
+            formatter = new Formatter("asdf.txt");
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File creation errror");
+        }
+    }
+
+    void addRecords()
+    {
+        formatter.format("%s%s%s", "19 ", "Davoleo ", "Felt blu");
+    }
+
+    void closeFile()
+    {
+        formatter.close();
+    }
+
+}
