20b4472f99118b0560cafd968805e48cde8b3687
Davoleo <davoleo2001@gmail.com>
2019-02-08 00:29:20 +0100

    Linked Lists

--- src/altro/Collections.java
+++ src/altro/Collections.java
@@ -14,12 +14,14 @@ public class Collections {
 
     public static void main(String[] args)
     {
+        //---------------------------------------------------------------------------------------------------
+        //ArrayLists
         //Array and list v1 initialization
         String[] things = {"eggs", "lasers", "hats", "pie"};
-        List<String> list = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
 
         //Adds the string in the first array to the list
-        list.addAll(Arrays.asList(things));
+        list1.addAll(Arrays.asList(things));
 
         //Array and list v2 initialization
         String[] otherThings = {"lasers", "hats"};
@@ -30,16 +32,70 @@ public class Collections {
             list2.add(s);
 
         //Prints out the Strings in the 1st list before editing the list
-        for (String s : list)
+        for (String s : list1)
             System.out.printf("%s ", s);
 
-        editList(list, list2);
+        editList(list1, list2);
         System.out.println();
 
         //Prints out the Strings in the 1st list after editing the list
-        for (String s : list)
+        for (String s : list1)
             System.out.printf("%s ", s);
 
+        //-----------------------------------------------------------------------------------------------------
+        //LinkedLists
+        String[] stuff = {"apples", "noobs", "pwnge", "bacon", "goat"};
+        List<String> list3 = new LinkedList<>();
+        for(String s : stuff)
+            list3.add(s);
+
+        String[] stuff2 = {"sausage", "bacon", "goat", "harrypotter"};
+        List<String> list4 = new LinkedList<>();
+        for (String s : stuff2)
+            list4.add(s);
+
+        list3.addAll(list4);
+        list4 = null;
+
+        printList(list3);
+        removeStuff(list3, 2, 5);
+        printList(list3);
+        printReversedList(list3);
+
+
+    }
+
+    /**
+     * It reverses the items list passed as a parameter
+     * @param list List that needs to be reversed
+     */
+    private static void printReversedList(List<String> list)
+    {
+        ListIterator<String> iterator = list.listIterator(list.size());
+        while (iterator.hasPrevious())
+            System.out.printf("%s", iterator.previous());
+    }
+
+    /**
+     * Removes the items of index between the lowerBound and the upperBound from the list
+     * @param list List the method is going to remove the items from
+     * @param from The lowest index (inclusive) of the item in the list that is going to be removed
+     * @param to The highest index (exclusive) of the item in the list that is going to be removed
+     */
+    private static void removeStuff(List<String> list, int from, int to)
+    {
+        list.subList(from, to).clear();
+    }
+
+    /**
+     * Prints our the list passed as a parameter
+     * @param list List that needs to be printed out
+     */
+    private static void printList(List<String> list)
+    {
+        for (String s : list)
+            System.out.printf("%s ", s);
+        System.out.println();
     }
 
     /**
